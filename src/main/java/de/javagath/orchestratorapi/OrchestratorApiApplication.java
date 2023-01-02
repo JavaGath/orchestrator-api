9258df107893cf5de33d1f44a2eb65e032da08a4
JavaGath <i.izrailtenko@web.de>
2023-01-02 20:17:36 +0100

    init spring project

---
+++ src/main/java/de/javagath/orchestratorapi/OrchestratorApiApplication.java
@@ -0,0 +1,13 @@
+package de.javagath.orchestratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/javagath/orchestratorapi/OrchestratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.javagath.orchestratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
